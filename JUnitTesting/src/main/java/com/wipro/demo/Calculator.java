e399d077f0d786ce18c5cb9b0cbc439320f2ce19
javeed512 <50737382+javeed512@users.noreply.github.com>
2022-07-01 12:26:52 +0530

    updates in calculator file

--- JUnitTesting/src/main/java/com/wipro/demo/Calculator.java
+++ JUnitTesting/src/main/java/com/wipro/demo/Calculator.java
@@ -4,10 +4,13 @@ public class Calculator {
 
 	public int add(int a, int b) {
 
+		System.out.println("hello friends")
 		return a + b;
 	}
 
 	public int sub(int a, int b) {
+		
+		System.out.println("thank you");
 
 		return a - b;
 	}
